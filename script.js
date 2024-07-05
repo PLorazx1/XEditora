22f7f8023b786fb1781512f8c1dc4d7e36e7c891
Paulo Loraschi <paulo.ss.loraschi@gmail.com>
2024-07-04 21:32:47 -0300

    função URL e chamadaAPI

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 // Constante a seguir precisa definir para a execução normal
 const IDbusca = 'Exemplo';
 
-const { getJson } = require("serpapi");
+function CriarURL(pesquisa){
+    let dominio = 'https://serpapi.com/search?engine=google_scholar&';
+    let pag = '&hl=pt-br&lr=pt-br&num=12';
+    let url = dominio + pesquisa + pag;
+    return url;
+}
 
+async function ChamadaAPI(url){
+    let resultado = await fetch(url);
+    console.log(resultado);
+}
 
-// function CriarURL(pesquisa){
-    //     let dominio = ;
-    // }
-    
 function Busca(){
     let Busca, URL;
     // Busca = document.getElementById(IDbusca).value;
     Busca = 'café';
-    if (Busca.length = 0){
+    if (Busca.length == 0 || Busca == ' '){
         // função para erros - a definir
         console.log('Chegou aki');
     } else {
         // pagina indicadora de pesquisa
-        // URL = CriarURL(Busca);
-        getJson({
-            engine: "google_scholar",
-            q: Busca,
-            api_key: "secret_api_key"
-        }, (json) => {
-            console.log(json["organic_results"]);
-        });
+        URL = CriarURL(Busca);
+        ChamadaAPI(URL);
     }
 }
 
-Busca()
+Busca();
