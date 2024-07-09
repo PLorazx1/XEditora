5400e829d480a7d7f583ecb711506fe94381d1b8
Paulo Loraschi <paulo.ss.loraschi@gmail.com>
2024-07-08 22:44:04 -0300

    Função tempo esgotado e salvando o json no client-side

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// constantes iniciais
+//constantes iniciais:
 const IDbusca = ''; //a definir
 const chaveAPI = 'ca0de54a49d6977b45e9e6c8857ee028e996d04de7f95bcea9f1ada77bc99039'; //sujeito a alteração
 const lingua = 'pt-br';
 const segsEspera = 10; //em segundos
+const msgTempEsg = 'Ops...\nPor algum motivo, sua pesquisa demorou muito, tente novamente mais tarde.';
 
 const { getJson } = require("serpapi");
 
-function tratamento(caminho){
+function tratamento(caminho) {
   fetch(caminho).then((response) => {
     response.json().then((dados) => {
       console.log(dados.organic_results)
@@ -14,7 +15,7 @@ function tratamento(caminho){
   })
 }
 
-function Busca(){
+function Busca() {
   let Busca;
   // Busca = document.getElementById(IDbusca).value;
   Busca = 'café';
@@ -26,8 +27,8 @@ function Busca(){
     // pagina indicadora de pesquisa
     //função de verificar o tempo:
     setTimeout(() => {
-      //função arrow para testar se chegou
-    }, segsEspera*1000);
+      if(localStorage.length == 0) alert(msgTempEsg);
+    }, segsEspera * 1000);
     //chamada da API:
     getJson({
       api_key: chaveAPI,
@@ -35,7 +36,10 @@ function Busca(){
       q: Busca,
       hl: lingua,
       num: "12"
-    }, (json) => { 
+    }, (json) => {
+      //salvando o json no client-side:
+      localStorage.setItem('resultados', JSON.stringify(json.organic_results));
+      localStorage.setItem('paginacao', JSON.stringify(json.serpapi_pagination));
       // função arrow para realizar o tratamento de dados
     });
   }
