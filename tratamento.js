5702bb33a568dc35165c5e4296846def9ce8e87f
Paulo Loraschi <paulo.ss.loraschi@gmail.com>
2024-07-08 23:10:10 -0300

    arquivo de estudos/testes

--- tratamento.js
+++ tratamento.js
@@ -1,21 +1,13 @@
-function PorFileSytem(){    
-    const fs = require("fs");
-
-    fs.readFile("result.json", "utf-8", (err, dados) => {
-        if (err) console.log(err);
-        else console.log(dados.json());
-    })
-
-}
-
 function PorFetch(){
     let div = document.querySelector("#resultados");
 
-    fetch("result.json").then((response) => {
-        response.json().then((resultados) => {
-            console.log(resultados)
+    fetch("https://dummyjson.com/c/9fa8-2872-4fb2-82e3").then((response) => {
+        response.json().then((resposta) => {
+            localStorage.setItem('resultados', JSON.stringify(resposta.organic_results));
+            localStorage.setItem('paginacao', JSON.stringify(resposta.serpapi_pagination));
         })
     });
-}
 
-PorFileSytem()
+    let variavel = localStorage.getItem('resultados');
+    div.innerHTML = variavel;
+}
